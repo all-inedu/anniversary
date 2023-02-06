06a020296b3008340987f0e7e6a0de03f6e919a5
Hafidz Annur Fanany <hafidz.bdt@gmail.com>
2023-02-06 12:46:26 +0700

    06/02/2023

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,4 +1,9 @@
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 <script>
     AOS.init();
-  </script>
+    $('.select').select2({
+        placeholder: "Your answer",
+        allowClear: true,
+        width: 'resolve'
+    });
+</script>
